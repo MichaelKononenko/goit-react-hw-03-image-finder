eac210651ba2915006cf47fb1055b6a58e22b370
Michael Kononenko <michael.kononenko101@gmail.com>
2022-12-23 00:41:04 +0200

    all bugs are fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,23 +17,29 @@ export class App extends Component{
         data: [],
         isLoading: false,
         showLoadMode: false,
+        noMorePhoto: false,
     }
 
 
-    async componentDidUpdate(_, prevState) {
+    async componentDidUpdate(_, prevState) {        
         if(prevState.toSearch === this.state.toSearch && prevState.page === this.state.page) return;
 
-        if(prevState.toSearch !== this.state.toSearch) this.setState({data: [], page: 1});
+        try{
         this.setState({showLoadMode: false, isLoading: true});
         const response = await axios.get(`?q=${this.state.toSearch}&page=${this.state.page}&key=${this.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
         this.setState({ data: [...this.state.data, ...response.data.hits], isLoading: false, showLoadMode: true});
+        }catch{
+            this.setState({noMorePhoto: true, isLoading: false});
+        }
       }
       
     onSubmit = event =>{
         event.preventDefault();
         const inputText = event.currentTarget.lastChild.value;
         if (this.state.toSearch === inputText) return;
+        if(this.state.toSearch !== inputText)  this.setState({data: [], page: 1});
         this.setState({toSearch: inputText});
+        event.currentTarget.reset();
     }
     
     loadMore = () => {
@@ -41,7 +47,7 @@ export class App extends Component{
     }
 
     render(){
-        const {isLoading, showLoadMode} = this.state;
+        const {isLoading, showLoadMode, noMorePhoto} = this.state;
         return(
         <div className="app">
         <GlobalStyle/>
@@ -49,6 +55,7 @@ export class App extends Component{
             <ImageGallery images={this.state.data}/>
             {showLoadMode && <Button loadMore={this.loadMore}/> }
             {isLoading && <Loader/>}
+            {noMorePhoto && <span  style={{margin: "0 auto"}}>Oops no more photo found</span>}
         </div>
         )    
     }
